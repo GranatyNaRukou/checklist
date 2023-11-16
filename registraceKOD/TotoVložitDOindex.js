57da1fa11b2018ae035dc9a09e520f567d29d352
GranatyNaRukou <144336378+GranatyNaRukou@users.noreply.github.com>
2023-11-16 08:20:25 +0100

    Update TotoVložitDOindex.js

--- registraceKOD/TotoVložitDOindex.js
+++ registraceKOD/TotoVložitDOindex.js
@@ -5,7 +5,7 @@ app.get('/register',(req, res) =>{
 app.post('/register',(req, res) =>{
   const { prezdivka, heslo, hesloznovu } = req.body;
 
-
+  
   connection.query('INSERT INTO register (uzivatelskeJmeno, heslo)VALUES (?, ?)',
   [prezdivka, hesloznovu],
   (err, result, fields) => {
@@ -17,5 +17,7 @@ app.post('/register',(req, res) =>{
     
   });
   res.render("login.ejs");
-
-});
+  
+  app.get('/login', (req, res) =>{
+    res.render('login.ejs')
+  });
